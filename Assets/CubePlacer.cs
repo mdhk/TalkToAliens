4a245789e6eb43f0fd80a5ad88397b4de8b23011
mdhk <mdhk@users.noreply.github.com>
2017-01-24 23:18:21 +0100

    alien sentences are now printed next to alien; alien + text is stuck to the viewport; new situations are generated by pressing the space bar

--- Assets/CubePlacer.cs
+++ Assets/CubePlacer.cs
@@ -13,6 +13,13 @@ public class CubePlacer : MonoBehaviour {
 	public GameObject duck2;
 	public GameObject frog1;
 	public GameObject frog2;
+	public GameObject subj;
+	public GameObject obj;
+	public GameObject alien;
+	public Camera MainCamera;
+	public int counter = 0;
+
+	public TextMesh sentence;
 
 	GameObject GetGameObject(string name, bool subject) {
 		GameObject obj;
@@ -57,27 +64,15 @@ public class CubePlacer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GrammarReader grammar = GetComponent<GrammarReader> ();
-		Situation sit = grammar.GetRandomSituation ();
-		Debug.Log (sit.Sentence);
-
-		Transform transform = GetComponent<Transform> ();
-		transform.position = new Vector3 (0, 0, 0);
-
-		var subj = GetGameObject (sit.Subj.Name, true);
-		var obj = GetGameObject (sit.Obj.Name, false);
-
-		subj.SetActive (true);
-		obj.SetActive (true);
-
-		subj.transform.Translate(new Vector3 (sit.Subj.Location.X*20, 1, SkipRiver(sit.Subj.Location.Y)*10));
-			obj.transform.Translate(new Vector3 (sit.Obj.Location.X*20, 1, SkipRiver(sit.Obj.Location.Y)*10));
-
-		Debug.Log (subj.transform.position.z);
-		Debug.Log (obj.transform.position.z);
-
+		// alien zit vast aan camera
+		GenerateSituation ();
+		alien.transform.parent = MainCamera.transform;
 	}
 
+
+	// TODO: ervoor zorgen dat entities niet in de rivier gaan staan en liefst niet uit beeld verdwijnen
+	// ik wilde 2 optellen bij alle Y-coordinaten groter gelijk aan 5 (want tussen 4 en 5 zit de rivier), maar dit werkt niet
+	// update: volgens mij werkt het toch wel?
 	int SkipRiver (int Y_loc) {
 		if (Y_loc >= 5) {
 			Y_loc = Y_loc + 2;
@@ -85,9 +80,54 @@ public class CubePlacer : MonoBehaviour {
 		return Y_loc;
 	}
 
+	void GenerateSituation() {
+		GrammarReader grammar = GetComponent<GrammarReader> ();
+
+		Situation sit = grammar.GetRandomSituation ();
+		// TODO: zorgen dat er een logische opvolging van situaties (?) is -- iig niet twee dezelfde situaties na elkaar
+		// elke zin opslaan in lijst en dan bij elke nieuwe situatie checken of ie niet al in de lijst staat ofzo?
+
+		counter += 1;
+
+		Transform transform = GetComponent<Transform> ();
+		transform.position = new Vector3 (0, 0, 0);
+
+		subj = GetGameObject (sit.Subj.Name, true);
+		obj = GetGameObject (sit.Obj.Name, false);
+
+		subj.SetActive (true);
+		obj.SetActive (true);
+
+		subj.transform.Translate(new Vector3 (sit.Subj.Location.X*20, 0, SkipRiver(sit.Subj.Location.Y)*10));
+		obj.transform.Translate(new Vector3 (sit.Obj.Location.X*20, 0, SkipRiver(sit.Obj.Location.Y)*10));
+
+		// TODO: update direction
+
+		Debug.Log ("=== SITUATION " + counter + " ===");
+		Debug.Log ("subj loc: (" + sit.Subj.Location.X + "," + sit.Subj.Location.Y + "), abs. pos.: (" + subj.transform.position.x 
+			+ "," + subj.transform.position.y + "," + subj.transform.position.z + ")");
+		Debug.Log ("obj loc: (" + sit.Obj.Location.X + "," + sit.Obj.Location.Y + "), " +
+			"abs. pos.: (" + obj.transform.position.x + "," + obj.transform.position.y + "," + obj.transform.position.z + ")");
+
+
+		sentence.text = sit.Sentence;
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown ("space")) {
+			// reset locatie van entities
+			Vector3 pos = new Vector3(0, 3, 0);
+			subj.transform.position = pos;
+			obj.transform.position = pos;
+
+			// deactiveer entities
+			subj.SetActive (false);
+			obj.SetActive (false);
+
+			// maak nieuwe situatie
+			GenerateSituation ();
+		}
 	}
 
 }
